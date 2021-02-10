ce524df0fee6adcc253aee32acaa4aa278a4be51
Ririko Nagaishi <riry.cucumber@gmail.com>
2021-02-11 02:55:16 +0900

    Initial Commit

---
+++ TechPod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechPod
+//
+//  Created by Ririko Nagaishi on 2021/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
